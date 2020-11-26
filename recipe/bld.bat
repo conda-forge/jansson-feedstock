d5f30af306258b5601342028b748549202326a23
Uwe L. Korn <uwe.korn@quantco.com>
2020-11-26 11:58:16 +0100

    Switch to CMake on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,11 @@
-call %BUILD_PREFIX%\Library\bin\run_autotools_clang_conda_build.bat
-if %ERRORLEVEL% neq 0 exit 1
+mkdir "%SRC_DIR%"\build
+pushd "%SRC_DIR%"\build
+
+cmake -G "Ninja" ^
+      -DBUILD_SHARED_LIBS=ON ^
+      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
+      ..
+if errorlevel 1 exit 1
+
+cmake --build . --target install --config Release
+if errorlevel 1 exit 1
