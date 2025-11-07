c1b8d15148c7cf147619ea843b55c9cd3f660284
Claudius Haag <claudius.haag@web.de>
2025-11-07 14:24:17 +0100

    Go back to cmake-shim

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,31 +1,20 @@
 #!/bin/bash
+
 set -exo pipefail
 
-mkdir -p "${SRC_DIR}/build"
-pushd "${SRC_DIR}/build"
-
-# This also sets the minimum CMake policy version to 3.5 for compatibility. Can be removed with >2.14.1
-cmake -G Ninja \
-      -DJANSSON_BUILD_SHARED_LIBS=ON \
-      -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
-      -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
-      -DCMAKE_BUILD_TYPE=Release \
-      ..
-
-# Build & install
-cmake --build . --target install --config Release -- -j${CPU_COUNT}
+autoreconf -i
+./configure --prefix=$PREFIX --disable-static
+[[ "$target_platform" == "win-64" ]] && patch_libtool
+make -j${CPU_COUNT}
 
 # Ignore this test
-cat > "${SRC_DIR}/test/suites/api/check-exports" <<'EOF'
+cat > test/suites/api/check-exports <<EOF
 #!/bin/sh
 exit 0
 EOF
-chmod +x "${SRC_DIR}/test/suites/api/check-exports"
+chmod +x test/suites/api/check-exports
 
-# Run tests unless cross-compiling.
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
-    echo "Running CMake 'check' target..."
-    cmake --build . --target check --config Release -- -j${CPU_COUNT}
+make install
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+    make check || { cat "${SRC_DIR}/test/test-suite.log"; exit 1; }
 fi
-
-popd
